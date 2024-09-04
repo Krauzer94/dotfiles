726858a53c3852b6c96d647b675798abbce40633
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2024-09-04 17:06:45 -0300

    Trim down configuration.nix comments

--- .flake/configuration.nix
+++ .flake/configuration.nix
@@ -13,13 +13,6 @@
   # Define your hostname
   networking.hostName = "nixos";
 
-  # Enables wireless support via wpa_supplicant
-  # networking.wireless.enable = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -42,7 +35,6 @@
   };
 
   # Enable the X11 windowing system
-  # You can disable this if you're only using the Wayland session
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment
@@ -66,16 +58,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager)
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’
   users.users.deck = {
@@ -85,7 +68,6 @@
     packages = with pkgs; [
       kdePackages.kate
       kdePackages.discover
-      #thunderbird
     ];
   };
 
@@ -98,39 +80,17 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile.
+  # To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
     git
-    # Do not forget to add an editor to edit configuration.nix
-    # The Nano editor is also installed by default
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
+  services.flatpak.enable = true; # Flatpaks
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable Flatpaks
-  services.flatpak.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether
-  # networking.firewall.enable = false;
-
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html)
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # Version compatibility wit installation
+  system.stateVersion = "24.05";
 
   # Enable OpenGL
   hardware.graphics = {
@@ -139,9 +99,7 @@
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
-
   hardware.nvidia = {
-
     # Modesetting is required
     modesetting.enable = true;
 

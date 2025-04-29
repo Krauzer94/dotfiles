ce2d2247785539931eab26916568762c133ec2fa
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-04-29 17:57:20 -0300

    Replace Mint with Kubuntu for desktop

--- .setup.sh
+++ .setup.sh
@@ -7,7 +7,7 @@ install_base() {
         steamdeck*)
             echo -e "\n Nothing to do here \n"
             ;;
-        mint*)
+        kubuntu*)
             sudo apt update && sudo apt install -y \
                 git mangohud steam-installer
             ;;
@@ -39,7 +39,7 @@ install_just () {
 remaining_apps() {
     HOST=$HOSTNAME
     case "$HOST" in
-        steamdeck*|mint*)
+        steamdeck*|kubuntu*)
             just installs-common
             ;;
         *)

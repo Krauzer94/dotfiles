da26a561777c59285b04f9045f5e4f99edf23179
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-04-29 10:29:20 -0300

    Replace Mint with Ubuntu for desktop

--- .setup.sh
+++ .setup.sh
@@ -7,7 +7,7 @@ install_git() {
         steamdeck*)
             echo -e "\n Nothing to do here \n"
             ;;
-        mint*|pop-os*)
+        ubuntu*|pop-os*)
             sudo apt update && sudo apt install -y \
                 git mangohud steam-installer
             ;;
@@ -39,7 +39,7 @@ install_just () {
 remaining_apps() {
     HOST=$HOSTNAME
     case "$HOST" in
-        steamdeck*|mint*|pop-os*)
+        steamdeck*|ubuntu*|pop-os*)
             just installs-common
             ;;
         *)

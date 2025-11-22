f6c45e754b2644f708b8f4983f89d1a62e0e1c48
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-11-22 16:59:52 -0300

    Remove support for Linux Mint

--- .setup.sh
+++ .setup.sh
@@ -10,7 +10,7 @@ install_base() {
         steamos)
             flatpak uninstall --all -y
             ;;
-        linuxmint|ubuntu)
+        ubuntu)
             sudo apt install -y git
             ;;
         *)
@@ -48,7 +48,7 @@ remaining_apps() {
         steamdeck)
             just installs-common
             ;;
-        linuxmint|ubuntu)
+        ubuntu)
             just installs-specific
             ;;
         *)

f9f2a4373ac0ec5d5055b33caf1e76aba4397b45
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-11-27 14:10:45 -0300

    Base packages for the WSL environment

--- .setup.sh
+++ .setup.sh
@@ -11,7 +11,7 @@ install_base() {
             flatpak uninstall --all -y
             ;;
         ubuntu)
-            sudo apt install -y git wget
+            sudo apt install -y git
             ;;
         *)
             echo -e "\t Unsupported system, operation failed... \n"
@@ -52,6 +52,7 @@ remaining_apps() {
             just installs-specific
             ;;
         *)
+            sudo apt install -y wget podman
             just setup-devenv
             ;;
     esac

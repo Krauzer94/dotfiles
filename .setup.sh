87864c0e466f4556ed34738311bc616ccfd532b3
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-02-02 15:53:28 -0300

    Migrate from just recipes to bash functions

--- .setup.sh
+++ .setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -euo pipefail
-log() {
-    printf "\n==> %s\n" "$1"
-}
+log() { printf "\n==> %s\n" "$1"; }
+
+# Global variables
+DISTRO="$(. /etc/os-release && echo "$ID")"
+HOST="${HOSTNAME:-$(hostname)}"
 
 # Install base packages
 installs_base() {
     log "Installing base packages"
 
     # Install based on distro
-    DISTRO=$(source /etc/os-release && echo "$ID")
     case "$DISTRO" in
         steamos)
             flatpak uninstall --all -y
@@ -34,35 +35,126 @@ setup_dotfiles() {
     rm -rdf ~/dotfiles && git restore .
 }
 
-# Install Just CLI
-install_just () {
-    log "Installing just CLI tool"
+# Installs common applications
+installs_common() {
+    log "Installing common applications"
 
-    # Ensure binary in $PATH
-    mkdir -p ~/.local/bin
-    curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
-    export PATH=$PATH:~/.local/bin
+    # Ensure app theming
+    setup_themes
+
+    # Flatpak apps to install
+    FLATPAK_APPS=(
+        com.dec05eba.gpu_screen_recorder
+        com.discordapp.Discord
+        com.github.tchx84.Flatseal
+        com.google.Chrome
+        com.rtosta.zapzap
+        com.spotify.Client
+        com.stremio.Stremio
+        com.visualstudio.code
+        io.github.flattool.Warehouse
+        io.missioncenter.MissionCenter
+        net.davidotek.pupgui2
+        org.bleachbit.BleachBit
+        org.flameshot.Flameshot
+        org.gimp.GIMP
+        org.kde.gwenview
+        org.kde.kcalc
+        org.mozilla.firefox
+        org.onlyoffice.desktopeditors
+        org.qbittorrent.qBittorrent
+        org.videolan.VLC
+    )
+
+    # Install all Flatpaks
+    flatpak install -y --noninteractive flathub "${FLATPAK_APPS[@]}" > /dev/null
+}
+
+# Installs distro specific apps
+installs_specific() {
+    log "Installing distro specific apps"
+
+    # Main packages to install
+    DISTRO_PACKAGES=(
+        mangohud
+        flatpak
+        steam
+        ufw
+    )
+
+    # Install based on distro
+    case "$DISTRO" in
+        ubuntu)
+            sudo dpkg --add-architecture i386
+            sudo apt update && sudo apt install -y "${DISTRO_PACKAGES[@]}"
+            sudo ubuntu-drivers install
+            sudo ufw enable
+            ;;
+        *)
+            log "Unsupported system, operation failed"
+            exit 1
+            ;;
+    esac
+
+    # Install remaining apps
+    installs_common
+}
+
+# Set up development environment
+setup_devenv() {
+    log "Setting up development environment"
+
+    # Ensure Github SSH connection
+    ssh-keygen -t ed25519 -C 13894059+Krauzer94@users.noreply.github.com
+    log "Generated SSH key"
+    cat ~/.ssh/id_ed25519.pub
+    git remote set-url origin git@github.com:Krauzer94/dotfiles.git
+}
+
+# Set up application theming
+setup_themes() {
+    # Create necessary folders
+    mkdir -p $HOME/{.themes,.icons}
+
+    # Copy system files over
+    cp -r /usr/share/themes/* $HOME/.themes/
+    cp -r /usr/share/icons/* $HOME/.icons/
 }
 
 # Install remaining apps
 remaining_apps() {
     # Install based on hostname
-    HOST=$HOSTNAME
     case "$HOST" in
         steamdeck)
-            just installs-common
+            installs_common
             ;;
         ubuntu)
-            just installs-specific
+            installs_specific
             ;;
         *)
-            just setup-devenv
+            setup_devenv
             ;;
     esac
 }
 
-# Execute all functions
-installs_base
-setup_dotfiles
-install_just
-remaining_apps
+# Execute all routines
+main() {
+    installs_base
+    setup_dotfiles
+    remaining_apps
+}
+
+# Enable function calling
+if [[ "$#" -eq 0 ]]; then
+    main
+else
+    for fn in "$@"; do
+        if declare -F "$fn" >/dev/null; then
+            log "Running $fn"
+            "$fn"
+        else
+            log "Error: function '$fn' not found" >&2
+            exit 1
+        fi
+    done
+fi
